f52063c9edf4f82d827167ce1d2aea7fa2b7bc9b
Edu <you@example.com>
2020-10-05 23:47:31 -0500

    Add @Get

--- lib/controller.ts
+++ lib/controller.ts
@@ -9,8 +9,33 @@ export function Controller(path : string = null){
     const setMetdata = {
       id: nanoid(),
       type: 'controller',
-      options: {
+      route: {
         path
+      }
+    };
+    if(hasMetadata){
+      target.prototype.metadata = {
+        ...target.prototype.metadata,
+        ...setMetdata
+      }
+      return;
+    }
+    target.prototype.metadata = setMetdata;
+  }
+}
+
+export function Get(path : string){
+  return (target : any) => {
+    const hasMetadata = Object.prototype.hasOwnProperty.call(
+      target.prototype, 
+      'metadata'
+    );
+    const setMetdata = {
+      id: nanoid(),
+      type: 'route',
+      route: {
+        path,
+        method: 'GET'
       }
     };
     if(hasMetadata){
--- test/controller.spec.ts
+++ test/controller.spec.ts
@@ -1,6 +1,6 @@
-import { Controller } from "../lib/controller";
+import { Controller, Get } from "../lib/controller";
 
-describe('Controller', () => {
+describe('@Controller', () => {
 
   test('When the @Controller is inserted, add metadata', () => {
     const obj = jest.fn();
@@ -12,16 +12,32 @@ describe('Controller', () => {
 
   test('When the @Controller path has value, set path', () => {
     const obj = jest.fn();
+    const path = '/test';
     const controller = Controller('/test');
     controller(obj);
-    expect(obj.prototype).toHaveProperty('metadata.options.path', '/test');
+    expect(obj.prototype).toHaveProperty('metadata.route.path', path);
   });
 
   test('When the @Controller path has not value, set path to null', () => {
     const obj = jest.fn();
     const controller = Controller();
     controller(obj);
-    expect(obj.prototype).toHaveProperty('metadata.options.path', null);
+    expect(obj.prototype).toHaveProperty('metadata.route.path', null);
+  });
+
+})
+
+describe('@Get', () => {
+
+  test('When the @Get is inserted, add metadata', () => {
+    const obj = jest.fn();
+    const path = '/test';
+    const get = Get(path);
+    get(obj);
+    expect(obj.prototype).toHaveProperty('metadata.id');
+    expect(obj.prototype).toHaveProperty('metadata.type', 'route');
+    expect(obj.prototype).toHaveProperty('metadata.route.path', path);
+    expect(obj.prototype).toHaveProperty('metadata.route.method', 'GET');
   });
 
 })
