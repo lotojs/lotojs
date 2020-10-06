223545491a8e7420d2c306758c6f8293d8cbc2b5
Edu <you@example.com>
2020-10-06 12:28:49 -0500

    Add function metadata setter

--- lib/controller.ts
+++ lib/controller.ts
@@ -24,51 +24,110 @@ export function Controller(path : string = null){
   }
 }
 
-export function Get(path : string){
-  return (target : any) => {
-    const hasMetadata = Object.prototype.hasOwnProperty.call(
-      target.prototype, 
-      'metadata'
+function addMetadataRoute(target : any, options : any = {}){
+  const hasMetadata = Object.prototype.hasOwnProperty.call(
+    target.prototype, 
+    'metadata'
+  );
+  const id = nanoid();
+  const path = options.path;
+  const method = options.method;
+  const type = 'route';
+  const setMetdata = {
+    type,
+    route: {
+      path,
+      method: [method]
+    }
+  };
+  if(hasMetadata){
+    const hasMetadataType = Object.prototype.hasOwnProperty.call(
+      target.prototype.metadata, 
+      'type'
     );
-    const id = nanoid();
-    const method = 'GET';
-    const type = 'route';
-    const setMetdata = {
-      type,
-      route: {
-        path,
-        method: [method]
-      }
-    };
-    if(hasMetadata){
-      const hasMetadataType = Object.prototype.hasOwnProperty.call(
-        target.prototype.metadata, 
-        'type'
-      );
-      if(hasMetadataType && target.prototype.metadata.type === type){
-        target.prototype.metadata = {
-          ...target.prototype.metadata,
-          ...{
-            route: {
-              ...target.prototype.metadata.route,
-              method: [
-                ...target.prototype.metadata.route.method,
-                method
-              ]
-            }
-          }
-        }
-        return;
-      }
+    if(hasMetadataType && target.prototype.metadata.type === type){
       target.prototype.metadata = {
         ...target.prototype.metadata,
-        ...setMetdata
+        ...{
+          route: {
+            ...target.prototype.metadata.route,
+            method: [
+              ...target.prototype.metadata.route.method,
+              method
+            ]
+          }
+        }
       }
       return;
     }
     target.prototype.metadata = {
-      id,
+      ...target.prototype.metadata,
       ...setMetdata
-    };
+    }
+    return;
+  }
+  target.prototype.metadata = {
+    id,
+    ...setMetdata
+  };
+}
+
+export function Get(path? : string){
+  return (target : any) => {
+    addMetadataRoute(
+      target,
+      {
+        path,
+        method: 'GET'
+      }
+    );
+  }
+}
+
+export function Post(path? : string){
+  return (target : any) => {
+    addMetadataRoute(
+      target,
+      {
+        path,
+        method: 'POST'
+      }
+    );
+  }
+}
+
+export function Put(path? : string){
+  return (target : any) => {
+    addMetadataRoute(
+      target,
+      {
+        path,
+        method: 'PUT'
+      }
+    );
+  }
+}
+
+export function Patch(path? : string){
+  return (target : any) => {
+    addMetadataRoute(
+      target,
+      {
+        path,
+        method: 'PATCH'
+      }
+    );
+  }
+}
+
+export function Delete(path? : string){
+  return (target : any) => {
+    addMetadataRoute(
+      target,
+      {
+        path,
+        method: 'DELETE'
+      }
+    );
   }
 }
