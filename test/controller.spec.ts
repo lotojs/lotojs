8bf1392b915d08f7fb3f33773f2d4045c0980d89
Edu <you@example.com>
2020-10-05 23:29:54 -0500

    Change test controller

--- test/controller.spec.ts
+++ test/controller.spec.ts
@@ -8,7 +8,20 @@ describe('Controller', () => {
     controller(obj);
     expect(obj.prototype).toHaveProperty('metadata.id');
     expect(obj.prototype).toHaveProperty('metadata.type', 'controller');
+  });
+
+  test('When the @Controller path has value, set path', () => {
+    const obj = jest.fn();
+    const controller = Controller('/test');
+    controller(obj);
     expect(obj.prototype).toHaveProperty('metadata.options.path', '/test');
+  });
+
+  test('When the @Controller path has not value, set path to null', () => {
+    const obj = jest.fn();
+    const controller = Controller();
+    controller(obj);
+    expect(obj.prototype).toHaveProperty('metadata.options.path', null);
   });
 
 })
